aeba36b258e548a049c13ecd6db89c49b6d76612
Juno-Cheng <chengjonathan2021@gmail.com>
2025-03-23 21:48:32 -0700

    feat: changes

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  basePath: "/juno-cheng.github.io", // Adjusted for GitHub Pages
-  output: "export", // Ensures static export for GitHub Pages
-  images: {
-    unoptimized: true, // Ensures images work correctly on GitHub Pages
-  },
   webpack(config) {
     // Add SVGR loader to handle SVG imports as React components
     config.module.rules.push({
